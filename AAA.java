688a17f4cd87f672e856d59b1f4365fd6f5030c6
shijiayua <2579638736@qq.com>
2018-10-11 10:52:04 +0800

    第一次测试

--- AAA.java
+++ AAA.java
@@ -5,4 +5,12 @@ package com.zxb.entity;
  */
 public class AAA {
     private int aaa;
+
+    public int getAaa() {
+        return aaa;
+    }
+
+    public void setAaa(int aaa) {
+        this.aaa = aaa;
+    }
 }
--- B.java
+++ B.java
@@ -5,4 +5,12 @@ package com.zxb.entity;
  */
 public class B {
     private int b;
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
 }
